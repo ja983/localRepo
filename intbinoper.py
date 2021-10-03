b5bbcc8ac90c01c8727f0160c4e1b000f7bfae8c
ja983 <91011523+ja983@users.noreply.github.com>
2021-10-03 11:53:58 +0530

    Update intbinoper.py

--- intbinoper.py
+++ intbinoper.py
@@ -12,16 +12,3 @@ div=a/b
 print("Division Operator:",div)
 mod=a%b
 print("Modulo Operator:",mod)
-
-a=int(input("Enter a:"))
-b=int(input("Enter b:"))
-add=a+b
-print("Addition Operator:",add)
-sub=a-b
-print("Subtraction Operator:",sub)
-mul=a*b
-print("Multiplication Operator:",mul)
-div=a/b
-print("Division Operator:",div)
-mod=a%b
-print("Modulo Operator:",mod)
